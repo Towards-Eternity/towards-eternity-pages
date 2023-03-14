b6f0d8e7f781fe34e63ca7ba820540e52feaf3c9
cyb3rsalih <mehmetsalihbindak@gmail.com>
2023-03-14 10:55:45 +0300

    visual improvement??

--- public/script.js
+++ public/script.js
@@ -4,24 +4,29 @@ const apps = {
     ios: "https://apps.apple.com/tr/app/kopgit-risale-inur-e%C4%9Fitimi/id1534780135?l=tr",
     android:
       "https://play.google.com/store/apps/details?id=com.kopgit.risaleinur",
+    logo:"kopgit.svg"
   },
   lamelifApp: {
     ios: "https://apps.apple.com/tr/app/lamelif-kuran-ve-tecvid-e%C4%9Fitim/id1541395117?l=tr",
     android: "https://play.google.com/store/apps/details?id=com.lamelif.kuran",
+    logo:"lamelif.svg"
   },
   namazApp: {
     ios: "https://apps.apple.com/tr/app/5-te-5-namaz/id1602490524?l=tr",
     android:
       "https://play.google.com/store/apps/details?id=com.sozlerkosku.beste5v2",
+    logo:"5te5.svg"
   },
   pusulamApp: {
     ios: "https://apps.apple.com/tr/app/pusulam-s%C3%BCnneti-hayata-ge%C3%A7ir/id1542536987?l=tr",
     android: "https://play.google.com/store/apps/details?id=com.sunnah.app",
+    logo:"pusulam.svg"
   },
   medresemApp: {
     ios: "https://apps.apple.com/tr/app/medresem-i-slami-e%C4%9Fitim/id1541506836?l=tr",
     android:
       "https://play.google.com/store/apps/details?id=com.sozlerkosku.internals",
+    logo:"sk.svg"
   },
 };
 
@@ -74,21 +79,21 @@ const addLink = (id, os) => {
 };
 
 // Add icon function with id parameter
-const addIcon = (id, os) => {
-  const app = apps[id]; // Get the app data
-  const link = document.getElementById(id); // Get the link
+// const addIcon = (id, os) => {
+//   const app = apps[id]; // Get the app data
+//   const link = document.getElementById(id); // Get the link
 
-  // Create the icon element with os store icon
-  const icon = document.createElement("i");
+//   // Create the icon element with os store icon
+//   const icon = document.createElement("img");
 
-  if (os === "ios") {
-    icon.classList.add("fa-brands", "fa-apple");
-  } else {
-    icon.classList.add("fa-brands", "fa-google-play");
-  }
+//   if (os === "ios") {
+//     icon.classList.add("fa-brands", "fa-apple");
+//   } else {
+//     icon.classList.add("fa-brands", "fa-google-play");
+//   }
 
-  link.prepend(icon); // Prepend the icon to the link
-};
+//   link.prepend(icon); // Prepend the icon to the link
+// };
 
 // Main function
 const main = () => {
@@ -105,19 +110,49 @@ const main = () => {
       const id = link.id; // Get the link's id
 
       addLink(id, deviceType); // Add the link
-      addIcon(id, deviceType); // Add the icon
+      // addIcon(id, deviceType); // Add the icon
     });
   }
 };
 
 // About Us Button Listener
-aboutUsButton.addEventListener("click", () => {
-  if (aboutUsText.style.display === "block") {
-    aboutUsText.style.display = "none";
-  } else {
-    aboutUsText.style.display = "block";
-  }
+const copyLinkBtn = document.querySelector('.copy-link');
+
+copyLinkBtn.addEventListener('click', function() {
+  const url = window.location.href;
+  navigator.clipboard.writeText(url).then(function() {
+    copyLinkBtn.textContent = 'Kopyalandı ✅';
+    setTimeout(function() {
+      copyLinkBtn.textContent = 'Linki Kopyala';
+    }, 2000);
+  });
 });
+
+async function copyLink(event) {
+  event.preventDefault();
+
+  const linkElement = event.target.parentNode;
+  const url = linkElement.getAttribute('href');
+
+ 
+  try {
+    // Write the URL to the clipboard
+    await navigator.clipboard.writeText(url);
+
+    // Show the floating notification
+    const notificationElement = document.getElementById('copy-notification');
+    notificationElement.innerHTML = 'Link copied!';
+    notificationElement.style.display = 'block';
+    
+    // Hide the floating notification after 3 seconds
+    setTimeout(() => {
+      notificationElement.style.display = 'none';
+    }, 1000);
+  } catch (error) {
+    console.error('Failed to copy link:', error);
+  }
+}
+
 
 
 // Call the main function
